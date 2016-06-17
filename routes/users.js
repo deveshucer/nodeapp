850ee4ce26c56e0d558bc63fd1fd8ebd73e343ad
Devesh <deveshucer@gmail.com>
2016-06-17 21:35:05 +0530

    added db connecion code

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
+
+// Retrieve
+// var MongoClient = require('mongodb').MongoClient;
+
+// // Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/nodeapp", function(err, db) {
+//   if(!err) {
+//     console.log("We are connected");
+//   }else{
+//     console.log("unable to connect" + err);
+//   }
+// });
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/nodeapp';
+
+var users = [];
+
+var findUsers = function(db, callback) {
+   var cursor =db.collection('users').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+        //  console.dir(doc);
+         users.push(doc);
+      } else {
+         callback();
+      }
+   });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  findUsers(db, function() {
+      db.close();
+  });
+});
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(JSON.stringify(users));
 });
 
 module.exports = router;
